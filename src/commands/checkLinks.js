d23ccea31bb76beb6c5981e51efad76573ca4cf5
Jonathan Diehl <jonathan.diehl@gmail.com>
2022-06-27 12:29:49 +0200

    bugfix: ignore code blocks

--- src/commands/checkLinks.js
+++ src/commands/checkLinks.js
@@ -3,9 +3,14 @@ const { existsSync, writeFileSync } = require('fs')
 const { make } = require('../lib/docsIndex')
 const logger = require('../lib/logger')
 
+// find all links in a document
 function getLinks(doc) {
+  // remove code blocks
+  // TODO: Improve this regex
+  let content = doc.content.replace(/```[^`]+```/g, '').replace(/`[^`]+`/g, '')
+
+  // extract links
   const regex = /\[([^\]]*)\]\(([^)]+)\)/
-  let { content } = doc
   const links = []
   while (true) {
     const match = regex.exec(content)
@@ -26,7 +31,7 @@ exports.checkLinks = async () => {
       // simplify linkUrl
       // TODO: also check anchors
       const linkUrl = link.url.replace(/#.*$/, '').replace(/\/$/, '')
-      if (linkUrl.length === 0) continue
+      if (linkUrl.trim().length === 0) continue
 
       // ignore external links
       // TODO: check external links
@@ -42,7 +47,7 @@ exports.checkLinks = async () => {
       // check docs
       const linkDoc = index[linkUrl]
       if (!linkDoc) {
-        logger.warn(`Link not found: ${linkUrl} ${doc.file} [${link.text}](${link.url})`)
+        logger.warn(`Link not found: ${doc.file} [${link.text}](${link.url})`)
       } else if (linkDoc.alias) {
         // logger.warn(`Link to alias: ${doc.file} [${link.text}](${link.url})`)
       }
